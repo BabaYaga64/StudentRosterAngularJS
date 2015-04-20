056a49dc178340988c1754454fb28db060dbc7e4
David Eliason & Bojana Skarich <student@epicodus.com>
2015-04-20 10:05:05 -0700

    Add controller file, add index.html file, add app.jsfile.

---
+++ js/StudentsController.js
@@ -0,0 +1,9 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope){
+    $scope.students = [
+        { name : "Sam Schmidt"},
+        { name : "Tom Schmidt"},
+        { name : "Jill Schmidt"},
+        { name : "Tony Schmidt"},
+        { name : "Zack Schmidt"},
+    ]
+});
